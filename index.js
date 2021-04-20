d889c52236fa3d5fd3c83d8d157cf56d757dbee5
Marady <maradyalexis@gmail.com>
2021-04-20 09:23:01 -0400

    commit tree 17a2a8c20c8c67269a4a0f84210bba244b3e0461

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(target =  'roller-skate') {
+    return `This Saturday, I want to ${target}!`
+}
+
+const mondayWork = function (work = 'go to the office') {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(idk = '*') {
+    return function (word = 'special') {
+        return `You are ${idk}${word}${idk}!`
+    }
+}
